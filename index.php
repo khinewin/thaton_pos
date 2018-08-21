0ebecd87ec6528bbac6550b5f39626c1c9ca7698
khinewin <khinewin123@gmail.com>
2018-08-21 11:03:48 +0630

    add

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php session_start() ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POS</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<?php include 'nav_bar.php' ?>
+
+
+<script src="bootstrap/js/jQuery.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
